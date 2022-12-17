ad4f6a18ffb6e98f277415f8de1aef5967c5efbb
kyousuke678 <119562080+kyousuke678@users.noreply.github.com>
2022-12-17 12:38:53 +0000

    add

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,23 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to books_path(@book)
+  end
+
+  def index
+  end
+
+  def show
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:name,:image, :introduction)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  get "homes/about" => "homes#about", as: "about"
+  root to: "homes#top"
+  resources :books, only: [:new, :index, :show]
+  resources :users, only: [:show, :edit]
 end
